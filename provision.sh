a3510310d217d299fbd40f7e780095c431d4e6b0
Jon Smajda <jon@smajda.com>
2015-06-13 12:30:46 -0500

    Not very productive start

--- provision.sh
+++ provision.sh
@@ -10,6 +10,15 @@ ln -sf /vagrant /home/vagrant/
 mkdir -p /var/www
 ln -sf /vagrant/pythonkc_site /var/www/pythonkc_site
 
+# install pip the hard way
+# chip, please fix. :)
+mkdir /home/vagrant/tmp
+cd /home/vagrant/tmp
+wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py
+python3 ez_setup.py
+wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py
+python3 get-pip.py
+
 if [[ -z "$(which ansible)" ]]; then
     echo "Installing Ansible..."
     aptitude install -y python3 python3-dev python3-pip ansible
